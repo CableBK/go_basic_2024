30b435a57d861152170811e70bc232aad9f1f8f9
CableBK <dev.witchapat@gmail.com>
2024-11-08 00:05:11 +0700

    ตัวแปร 20241108

--- go_basic_2024/main.go
+++ go_basic_2024/main.go
@@ -1,6 +1,14 @@
 package main
 
 /*พีธีไหว้ครู 20241106*/
-func main() {
+/* func main() {
 	println("Hello Go")
+} */
+/*เขียนสั้น ๆ ยังไงไม่รู้ 20241107*/
+/*ตัวแปร 20241108*/
+func main() {
+	var number int16 = 44
+	var num int32
+	println(number)
+	println(num)
 }
